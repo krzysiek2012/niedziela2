79eb8bedee80179a95ff0271b019aadbdb9423a9
krzysiek2012 <olszewski.krz@gmail.com>
2019-12-13 20:09:21 +0100

    hhh dalej testuje, nie wiem co mi wyjdzie z tego gowna

---
+++ hhhh.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+licznik=0
+for linia in file("lorem.txt"):
+    if "bash" in linia:
+        licznik=licznik + 1
+print(licznik)
+
+licznik=0
+for linia in file("lorem.txt"):
+    if len("linia")>100 in linia:
+        licznik=licznik + 1
+print(licznik)
+
+
+import re
+s="[0-9]"
+r="6"
+licznik=0
+for linia in file("lorem.txt"):
+    if re.search(s,linia):
+        licznik=licznik + 1
+print(licznik)
