9709257f5766ed6caf0197c98f75d6d24fbb9f08
krzysiek2012 <olszewski.krz@gmail.com>
2019-12-22 20:46:59 +0100

    wersja poprawiona, ta z try, teraz dziala

--- ifff.py
+++ ifff.py
@@ -5,9 +5,9 @@ while True:
     reply = input('wpisz tekst:')
     if reply == 'stop': break
     try:
-        num = int(reply
+        num = int(reply)
     except:
-            print('niepoprawne! ' *5)
+        print('niepoprawne! ' *5)
     else:
         print(int(reply)**2)
 print('kuniec')
